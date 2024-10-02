fa73740b437188236b355e4c0f66803d2457d1ff
vpironski <vpironski@gmail.com>
2024-10-02 09:34:36 +0300

    Chancing for to for-each

--- java/funding-raised/src/main/java/com/checkr/interviews/FundingRaised.java
+++ java/funding-raised/src/main/java/com/checkr/interviews/FundingRaised.java
@@ -21,9 +21,9 @@ public class FundingRaised {
         if(options.containsKey("company_name")) {
             List<String[]> results = new ArrayList<> ();
 
-            for(int i = 0; i < csvData.size(); i++) {
-                if(csvData.get(i)[1].equals(options.get("company_name"))) {
-                    results.add(csvData.get(i));
+            for (String[] csvDatum : csvData) {
+                if (csvDatum[1].equals(options.get("company_name"))) {
+                    results.add(csvDatum);
                 }
             }
             csvData = results;
@@ -32,9 +32,9 @@ public class FundingRaised {
         if(options.containsKey("city")) {
             List<String[]> results = new ArrayList<> ();
 
-            for(int i = 0; i < csvData.size(); i++) {
-                if(csvData.get(i)[4].equals(options.get("city"))) {
-                    results.add(csvData.get(i));
+            for (String[] csvDatum : csvData) {
+                if (csvDatum[4].equals(options.get("city"))) {
+                    results.add(csvDatum);
                 }
             }
             csvData = results;
@@ -43,9 +43,9 @@ public class FundingRaised {
         if(options.containsKey("state")) {
             List<String[]> results = new ArrayList<> ();
 
-            for(int i = 0; i < csvData.size(); i++) {
-                if(csvData.get(i)[5].equals(options.get("state"))) {
-                    results.add(csvData.get(i));
+            for (String[] csvDatum : csvData) {
+                if (csvDatum[5].equals(options.get("state"))) {
+                    results.add(csvDatum);
                 }
             }
             csvData = results;
@@ -54,9 +54,9 @@ public class FundingRaised {
         if(options.containsKey("round")) {
             List<String[]> results = new ArrayList<> ();
 
-            for(int i = 0; i < csvData.size(); i++) {
-                if(csvData.get(i)[9].equals(options.get("round"))) {
-                    results.add(csvData.get(i));
+            for (String[] csvDatum : csvData) {
+                if (csvDatum[9].equals(options.get("round"))) {
+                    results.add(csvDatum);
                 }
             }
             csvData = results;
@@ -64,18 +64,18 @@ public class FundingRaised {
 
         List<Map<String, String>> output = new ArrayList<>();
 
-        for(int i = 0; i < csvData.size(); i++) {
-            Map<String, String> mapped = new HashMap<String, String> ();
-            mapped.put("permalink", csvData.get(i)[0]);
-            mapped.put("company_name", csvData.get(i)[1]);
-            mapped.put("number_employees", csvData.get(i)[2]);
-            mapped.put("category", csvData.get(i)[3]);
-            mapped.put("city", csvData.get(i)[4]);
-            mapped.put("state", csvData.get(i)[5]);
-            mapped.put("funded_date", csvData.get(i)[6]);
-            mapped.put("raised_amount", csvData.get(i)[7]);
-            mapped.put("raised_currency", csvData.get(i)[8]);
-            mapped.put("round", csvData.get(i)[9]);
+        for (String[] csvDatum : csvData) {
+            Map<String, String> mapped = new HashMap<>();
+            mapped.put("permalink", csvDatum[0]);
+            mapped.put("company_name", csvDatum[1]);
+            mapped.put("number_employees", csvDatum[2]);
+            mapped.put("category", csvDatum[3]);
+            mapped.put("city", csvDatum[4]);
+            mapped.put("state", csvDatum[5]);
+            mapped.put("funded_date", csvDatum[6]);
+            mapped.put("raised_amount", csvDatum[7]);
+            mapped.put("raised_currency", csvDatum[8]);
+            mapped.put("round", csvDatum[9]);
             output.add(mapped);
         }
 
